ad0ba92f6a2a6209ab42d76ddab73a4f3bd102a0
Subrata Mondal <subratamondal1029@outlook.com>
2024-11-18 09:22:43 +0530

    add plyer route and modify some route

--- frontend/src/route/index.jsx
+++ frontend/src/route/index.jsx
@@ -4,22 +4,23 @@ import {
   Route,
 } from "react-router-dom";
 import App from "../App";
-import { Home, Auth, Error } from "../pages";
+import { Home, Auth, Error, Player } from "../pages";
 import { SecureRoute } from "../components";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route path="login" element={<Auth type="l" />}></Route>
+      <Route path="register" element={<Auth type="r" />}></Route>
+      <Route path="" element={<Home />}></Route>
       <Route
-        path=""
+        path="player/:id"
         element={
           <SecureRoute authRequired>
-            <Home />
+            <Player />
           </SecureRoute>
         }
       ></Route>
-      <Route path="login" element={<Auth type="l" />}></Route>
-      <Route path="register" element={<Auth type="r" />}></Route>
     </Route>
   )
 );
