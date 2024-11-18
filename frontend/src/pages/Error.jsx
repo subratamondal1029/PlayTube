9154fb673c0643c67d286f35944a9836548d3087
Subrata Mondal <subratamondal1029@outlook.com>
2024-11-18 09:20:57 +0530

    give styling to error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { useLocation, useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
+import "../styles/Error.css";
 
-const Error = () => {
+const ErrorComponent = () => {
   const error = useRouteError();
-  console.log(error);
 
-  return <div>{error.data}</div>;
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="error-container">
+      <h1 className="error-code">{error?.status || 500}</h1>
+      <p className="error-message">
+        {error?.statusText || "Internal Server Error"}
+      </p>
+      <button className="back-button" onClick={handleBack}>
+        Go Back
+      </button>
+    </div>
+  );
 };
 
-export default Error;
+export default ErrorComponent;
