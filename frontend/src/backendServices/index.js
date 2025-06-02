e58541277ef1951b15055b52cefcf1161202bd83
Subrata Mondal <subratamondal@tutanota.com>
2025-06-02 11:47:52 +0530

    add all backend in main branch

--- frontend/src/backendServices/index.js
+++
@@ -1,4 +0,0 @@
-import { authService } from "./authService";
-import { videoService } from "./videoService";
-import { commentService } from "./commentService";
-export { authService, videoService, commentService };
--- frontend/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import { lazy } from "react";
-
-const Home = lazy(() => import("./Home"));
-const Auth = lazy(() => import("./Auth"));
-import Error from "./Error";
-const Player = lazy(() => import("./Player"));
-
-export { Home, Auth, Error, Player };
